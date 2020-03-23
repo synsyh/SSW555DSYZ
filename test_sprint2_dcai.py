b2961f7b65f92d46cd551c9e2284209ec166d7cd
cdp2323 <57602737+cdp2323@users.noreply.github.com>
2020-03-23 12:33:42 -0400

    Danping Cai Test

--- test_sprint2_dcai.py
+++ test_sprint2_dcai.py
@@ -1,22 +1,17 @@
 import unittest
-
+import geddata
 from dcai_sprint2 import birth_before_death
 from dcai_sprint2 import marriage_before_divorce
 
 
-def test_US03(self):
-    p = GEDParser('res/US03.ged')
-    p.parser()
-    self.assertEqual(birth_before_death(p),
-                     'ERROR: INDIVIDUAL: US03: birthday should occur before the death of a person')
+class TestSprint2(unittest.TestCase):
+    def test_US03(self):
+        inds, fams = geddata.get_inds_fams('res/US03.ged')
+        self.assertEqual(birth_before_death(fams, inds), 'ERROR: birthday should occur before the death of a person!')
 
-
-def test_US04(self):
-    p = GEDParser('res/US04.ged')
-    p.parser()
-    self.assertEqual(marriage_before_divorce(p),
-                     "ERROR: FAMILY: US04: marriage should occur before divorce of spouses")
-
+    def test_US04(self):
+        inds, fams = geddata.get_inds_fams('res/US04.ged')
+        self.assertEqual(marriage_before_divorce(fams, inds), 'ERROR: marriage should occur before divorce of spouses')
 
 
 if __name__ == '__main__':
